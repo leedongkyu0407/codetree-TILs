d91d28e6f7cf469d3325266064955e87fafdb3ab
leedongkyu0407 <54134100+leedongkyu0407@users.noreply.github.com>
2024-03-05 17:16:36 +0900

    Title: 바구니 안의 사탕 2, Time: 121ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240305/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,23 @@
+n, k = map(int, input().split())
+MAX_INT = 101
+maps = [0]*MAX_INT
+
+for _ in range(n):
+    candy, position = map(int, input().split())
+    maps[position] = candy
+
+
+max_candy = 0
+max_idx = 0
+#중심점
+for i in range(k, MAX_INT-k):
+    sum_candy = 0
+    for j in range(i-k, i+k+1):
+        if maps[j]:
+            sum_candy += maps[j]
+    
+    if max_candy < sum_candy:
+        max_candy = sum_candy
+        max_idx = i
+        
+print(max_candy)
