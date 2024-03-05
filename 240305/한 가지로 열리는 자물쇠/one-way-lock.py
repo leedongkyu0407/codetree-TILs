7ce768d228f5a67464237c5c2d6855ee53dadc04
leedongkyu0407 <54134100+leedongkyu0407@users.noreply.github.com>
2024-03-05 18:22:13 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 183ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240305/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -0,0 +1,12 @@
+n = int(input())
+a, b, c = map(int, input().split())
+
+cnt = 0
+#가능한 가짓수
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        for k in range(1, n+1):
+            if abs(a-i) <=2 or abs(b-j) <=2 or abs(c-k) <= 2:
+                cnt += 1            
+
+print(cnt)
