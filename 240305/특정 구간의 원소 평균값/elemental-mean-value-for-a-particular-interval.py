116b2ce9f80a62d38459bec3e6af68218ce87b4a
leedongkyu0407 <54134100+leedongkyu0407@users.noreply.github.com>
2024-03-05 15:43:04 +0900

    Title: 특정 구간의 원소 평균값, Time: 158ms, Memory: 25MB, Status: Passed - CodeTree

--- 240305/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 240305/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -2,17 +2,19 @@ n = int(input())
 n_list = list(map(int, input().split()))
 
 #[1, 1], [2, 2]...[n, n]과 같이 구간 안에 원소가 하나일 경우 조건 만족
-ans = n
+ans = 0
 #시작점
 for i in range(n):
-    #구간 길이
-    for j in range(n-i):
+    #끝점
+    for j in range(i, n):
         s = 0
         avg = 0
-        for k in range(i, i+j):
+        #구간 시작점과 끝점
+        for k in range(i, j+1):
             s += n_list[k]
-        avg = s/(j+1)
-        if avg in n_list[i:i+j]:
+        avg = s/(j+1-i)
+        if avg in n_list[i:j+1]:
+            
             ans += 1
         
 
