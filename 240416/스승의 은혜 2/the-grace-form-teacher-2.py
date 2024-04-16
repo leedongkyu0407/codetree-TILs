ef72a49fa390c6d4e650a232821980a6aa23c828
leedongkyu0407 <54134100+leedongkyu0407@users.noreply.github.com>
2024-04-16 14:23:15 +0900

    Title: 스승의 은혜 2, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

--- 240416/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240416/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -4,6 +4,7 @@ for _ in range(n):
     p = int(input())
     price.append(p)
 
+#선물 가격 정렬
 price.sort()
 ans = 0
 
@@ -12,13 +13,16 @@ for i in range(n):
     pi = price[i]//2
     people = 0
     b = B
+
+    #가장 많은 학생에게 선물을 줄 때의 학생 수
+    
     for j in range(n):
         if i == j:
             p = pi
         else:
             p = price[j]
         
-        
+    #p = 선물 가격 / b = 현재 남은 돈    
         if b - p >= 0:
             b -= p
             people += 1
