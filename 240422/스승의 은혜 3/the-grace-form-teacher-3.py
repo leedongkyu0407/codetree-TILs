219cb1f4377d6c1ee6adb7478ce0affd077faf09
leedongkyu0407 <54134100+leedongkyu0407@users.noreply.github.com>
2024-04-22 20:36:44 +0900

    Title: 스승의 은혜 3, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240422/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240422/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -7,8 +7,9 @@ for _ in range(n):
     p, s = map(int, input().split())
     prices_ship.append((p,s))
 
-prices_ship.sort(key=lambda x:(x[0]+x[1]))
-
+prices_ship.sort(key=lambda x:(x[0]+x[1], x[0]))
+print(prices_ship)
+#선물 가능한 학생의 최대 명수
 ans = 0
 for i in range(n):
     #절반 가격으로 할 선물 선택
@@ -17,8 +18,9 @@ for i in range(n):
     cnt = 0
     for j in range(n):
         if i == j:
-            pay = half+prices_ship[j][1]
-        pay += prices_ship[j][0]+prices_ship[j][1]
+            pay += half+prices_ship[j][1]
+        else:
+            pay += prices_ship[j][0]+prices_ship[j][1]
         cnt += 1
         
         if pay > b:
