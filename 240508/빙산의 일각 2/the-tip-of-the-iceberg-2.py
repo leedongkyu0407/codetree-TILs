9155f86fbc10168e8d0e0f8a50fed80bd1f92db2
leedongkyu0407 <54134100+leedongkyu0407@users.noreply.github.com>
2024-05-08 15:26:38 +0900

    Title: 빙산의 일각 2, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240508/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 240508/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -5,22 +5,30 @@ for _ in range(n):
     h = int(input())
     heights.append(h)
 
+#빙산이 바다보다 높으면 True
+def over(height, sea):
+    return height > sea
+
 ans = 0
 #해수면 높이 최댓값은 1000
 for i in range(1, 1001):
+    #모든 빙산이 해수면에 잠기면 종료
+    if max(heights) < i:
+        break
     sea_level = i
     cnt = 0
-    #앞선 빙산이 물위로 드러나 있는지
-    is_over = True
+    #첫 빙산이 해수면보다 높은지
+    is_over = over(heights[0], sea_level)
     for j in range(n):  
         if is_over:
             #빙산 한 덩어리가 끝나는 지점
-            if heights[j] <= sea_level:
+            if over(heights[j], sea_level) == False:
                 is_over = False
                 cnt += 1
+                
         else:
             #빙산 한 덩어리가 시작하는 지점
-            if heights[j] > sea_level:
+            if over(heights[j], sea_level) == True:
                 is_over = True
             
     #n번째 빙산이 마지막 빙산 덩어리에 포함되어 있는 경우
