94453cba3ae038f1c85a6c20014f06a629739334
leedongkyu0407 <54134100+leedongkyu0407@users.noreply.github.com>
2024-09-17 01:27:22 +0900

    Title: 팰린드롬 수 찾기, Time: 174ms, Memory: 25MB, Status: Passed - CodeTree

--- 240917/팰린드롬 수 찾기/find-the-number-of-palindrome.py
+++ 240917/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -5,7 +5,7 @@ num1 = int(x)
 num2 = int(y)
 
 ans = 0
-for n in range(num1-1, num2+1):
+for n in range(num1, num2+1):
     n_str = str(n)
     n_reversed = n_str[::-1]
     if(n_str == n_reversed):
