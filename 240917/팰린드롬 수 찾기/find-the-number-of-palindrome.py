8f3206b970a888b953f54b1f889816aed89c2fe3
leedongkyu0407 <54134100+leedongkyu0407@users.noreply.github.com>
2024-09-17 01:23:44 +0900

    Title: 팰린드롬 수 찾기, Time: 201ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240917/팰린드롬 수 찾기/find-the-number-of-palindrome.py
+++ 240917/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -2,21 +2,14 @@ import sys
 from collections import deque
 x, y = input().split()
 
-stack = deque()
 num1 = int(x)
 num2 = int(y)
 
 ans = 0
 for n in range(num1, num2):
     n_str = str(n)
-    mid = len(n_str)//2
-    front = n_str[:mid]
-    if(len(n_str)%2==1):
-        back = n_str[mid+1:]
-    else:
-        back = n_str[mid:]
-
-    if(front == back[::-1]):
+    n_reversed = n_str[::-1]
+    if(n_str == n_reversed):
         ans += 1
-    
+
 print(ans)
