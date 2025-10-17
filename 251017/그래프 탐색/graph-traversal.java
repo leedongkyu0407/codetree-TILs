1e0a6c480274e66d3bb267022fec0258294ca30d
DONGGYU LEE <use02012@gmail.com>
2025-10-17 21:57:05 +0900

    Title: 그래프 탐색, Time: 456ms, Memory: 23MB, Status: Passed - Codetree

---
+++ 251017/그래프 탐색/graph-traversal.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class Main {
+    static List<List<Integer>> graph = new ArrayList<>();
+    static int n, m; 
+    static boolean[] visited;
+    static int ans = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int i=0;i<=n;i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+
+        visited = new boolean[n+1];
+        int a=0, b=0;
+        for (int i = 0; i < m; i++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        dfs(1);
+        for(int i=2;i<=n;i++) {
+            if (visited[i]) {
+                ans++;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    private static void dfs(int start) {
+        for (int i=1;i<=graph.get(start).size();i++) {
+            int tmp = graph.get(start).get(i-1);
+            if(!visited[tmp]) {
+                visited[tmp] = true;
+                dfs(tmp);
+            }
+        }
+    }
+}
