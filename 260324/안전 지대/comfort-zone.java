ed2e93d539e11d88ffafdfe105aed39cc9d3b37e
DONGGYU LEE <use02012@gmail.com>
2026-03-24 15:28:35 +0900

    Title: 안전 지대, Time: 91ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260324/안전 지대/comfort-zone.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static int n, m, maxK;
+    private static int[][] board;
+    private static boolean[][] visited;
+    private static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException{
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        n = Integer.parseInt(st.nextToken());
+    }
+}
