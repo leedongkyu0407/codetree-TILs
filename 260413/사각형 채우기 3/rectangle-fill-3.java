a56cd99ee3338e23e54f579a20d9776b84fb6af0
DONGGYU LEE <use02012@gmail.com>
2026-04-13 16:15:59 +0900

    Title: 사각형 채우기 3, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260413/사각형 채우기 3/rectangle-fill-3.java
+++ 260413/사각형 채우기 3/rectangle-fill-3.java
@@ -7,10 +7,14 @@ public class Main {
         int n = Integer.parseInt(bf.readLine());
         int[] dp = new int[1001];
 
+        dp[0] = 1;
         dp[1] = 2;
         dp[2] = 7;
+        int sum = 1;
+
         for(int i=3;i<=n;i++) {
-            dp[i] = ((dp[i-2]*3) + (dp[i-1]*2) + 2)%1000000007;
+            dp[i] = ((dp[i-2]*3) + (dp[i-1]*2) + 2*sum)%1000000007;
+            sum=(sum+dp[i-2])%1000000007;
         } 
 
         System.out.println(dp[n]);
