e2c818442894eaf698aca6d39eeaa6f2253d32a4
DONGGYU LEE <use02012@gmail.com>
2026-04-21 18:37:37 +0900

    Title: 2차원 최대 증가 수열, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260421/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    private static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    private static int n, m;
+    private static int[][] board, dp;
+
+    public static void main(String[] args) throws IOException{
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        board = new int[n][m];
+        dp = new int[n][m];
+
+        for(int i=0;i<n;i++) {
+            st = new StringTokenizer(bf.readLine());
+            for(int j=0;j<n;j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i=0;i<n;i++) {
+            Arrays.fill(dp[i], Integer.MIN_VALUE);
+        }
+
+        for(int i=0;i<n;i++) {
+            dp[0][i] = 1;
+            dp[i][0] = 1;
+        }
+
+        for(int i=1;i<n;i++) {
+            for(int j=1;j<n;j++) {
+                for(int k=0;k<i;k++) {
+                    for (int l=0;l<j;l++) {
+                        if (board[i][j] <= board[k][l]) continue;
+                        dp[i][j] = Math.max(dp[k][l]+1, dp[i][j]);
+                    }
+                }
+            }
+        }
+
+        int ans = 0;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                ans = Math.max(dp[i][j], ans);
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
