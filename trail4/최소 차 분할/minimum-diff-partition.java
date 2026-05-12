86447ca475c64c470b3f8ed81cfa63bec47eaf49
DONGGYU LEE <use02012@gmail.com>
2026-05-12 18:51:58 +0900

    [Passed] 최소 차 분할 (92ms, 9MB)

--- trail4/최소 차 분할/minimum-diff-partition.java
+++ trail4/최소 차 분할/minimum-diff-partition.java
@@ -5,7 +5,7 @@ public class Main {
     private static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
     private static int n, sumA;
     private static int[] nums;
-    private static boolean[] dp;
+    private static boolean[][] dp;
 
     public static void main(String[] args) throws IOException{
         n = Integer.parseInt(bf.readLine());
@@ -16,25 +16,45 @@ public class Main {
             nums[i] = Integer.parseInt(st.nextToken());
             sumA += nums[i];
         }
-        dp = new boolean[sumA+1];
-        dp[0] = true;
-        System.out.println(solve());
+        dp = new boolean[n+1][sumA+1];
+        dp[0][0] = true;
+        System.out.println(solve2());
     }
 
-    private static int solve() {
+    private static int solve2() {
         for(int i=1;i<=n;i++) {
-            for(int j=sumA;j>=nums[i];j--) {
-                if (!dp[j-nums[i]]) continue;
-                dp[j] = true;
+            for(int j=1;j<=sumA;j++) {
+                if (j >= nums[i] && dp[i-1][j-nums[i]])
+                    dp[i][j] = true;
+                
+                if (dp[i-1][j])
+                    dp[i][j] = true;
             }
         }
 
         int answer = 100000;
 
         for(int i=1;i<=sumA;i++) {
-            if (dp[i])
+            if (dp[n][i])
                 answer = Math.min(answer, Math.abs(sumA-i-i));
         }
         return answer;
     }
+
+    // private static int solve() {
+    //     for(int i=1;i<=n;i++) {
+    //         for(int j=sumA;j>=nums[i];j--) {
+    //             if (!dp[j-nums[i]]) continue;
+    //             dp[j] = true;
+    //         }
+    //     }
+
+    //     int answer = 100000;
+
+    //     for(int i=1;i<=sumA;i++) {
+    //         if (dp[i])
+    //             answer = Math.min(answer, Math.abs(sumA-i-i));
+    //     }
+    //     return answer;
+    // }
 }
