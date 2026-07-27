0aa8ba6872d8e6b2811f51279f877fe3f6144a21
DONGGYU LEE <use02012@gmail.com>
2026-07-27 15:58:55 +0900

    [Passed] 쌓인 숫자의 순차적 이동 (96ms, 9MB)

--- trail4/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.java
+++ trail4/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.java
@@ -4,10 +4,8 @@ import java.util.*;
 public class Main {
     private static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
     private static int n, m;
-    
-    private static Map<Pos, Stack<Integer>> grid = new HashMap<>();
-    
-    private static Map<Integer, Pos> numPos = new HashMap<>();
+    private static Map<Pos, List<Integer>> hm = new HashMap<>();
+    private static Map<Integer, Pos> posMap = new HashMap<>();
     
     private static int[] orders;
     private static int[][] deltas = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}, {-1, 1}, {1, -1}, {1, 1}, {-1, -1}};
@@ -18,73 +16,71 @@ public class Main {
         print();
     }
 
+    private static void print() {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                Pos p = new Pos(i, j);
+                List<Integer> al = hm.get(p);
+                if(al.isEmpty()) {
+                    sb.append("None");
+                } else {
+                    for(int k=al.size()-1;k>=0;k--) {
+                        sb.append(al.get(k)).append(" ");
+                    }
+                }
+                sb.append("\n");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
     private static void solve() {
-        for(int i=0; i<m; i++) {
-            int nowNum = orders[i];
-            
-            Pos currentPos = numPos.get(nowNum);
-            
-            if(currentPos == null) continue;
-            
-            Pos nextPos = findMaxPos(currentPos);
-            
-            if(nextPos == null) continue;
-            
-            Stack<Integer> currentStack = grid.get(currentPos);
-            Stack<Integer> temp = new Stack<>();
-            
-            while(!currentStack.isEmpty()) {
-                int num = currentStack.pop();
-                temp.push(num);
-                if(num == nowNum) break;
-            }
-            
-            if(!grid.containsKey(nextPos)) {
-                grid.put(nextPos, new Stack<>());
-            }
-            
-            Stack<Integer> nextStack = grid.get(nextPos);
-            
-            while(!temp.isEmpty()) {
-                int num = temp.pop();
-                nextStack.push(num);
-                numPos.put(num, nextPos);
+        for(int i=0;i<m;i++) {
+            Pos nowPos = posMap.get(orders[i]);
+            Pos nextPos = findMax(orders[i]);
+            if(nextPos!=null) {
+                move(nowPos, nextPos, orders[i]);
             }
         }
     }
 
-    private static Pos findMaxPos(Pos p) {
-        int x = p.x;
-        int y = p.y;
-        
-        Pos maxPos = null;
+    private static void move(Pos p, Pos nextP, int num) {
+        List<Integer> currentList = hm.get(p);
+        int idx = currentList.indexOf(num);
+        List<Integer> toMoveList = new ArrayList<>(currentList.subList(idx, currentList.size()));
+
+        hm.get(p).removeAll(toMoveList);
+        hm.get(nextP).addAll(toMoveList);
+
+        for(Integer n : toMoveList) {
+            posMap.put(n, nextP);
+        }
+    }
+
+    private static Pos findMax(int num) {
+        Pos nowP = posMap.get(num);
+        Pos maxP = null;
         int maxNum = 0;
-        
+
         for(int[] delta: deltas) {
-            int nx = x + delta[0];
-            int ny = y + delta[1];
-            
+            int nx = nowP.x+delta[0];
+            int ny = nowP.y+delta[1];
             if(!inRange(nx, ny)) continue;
             
-            Pos checkPos = new Pos(nx, ny);
-            if(!grid.containsKey(checkPos)) continue;
-            
-            Stack<Integer> stack = grid.get(checkPos);
-            if(stack.isEmpty()) continue;
-            
-            int maxInStack = 0;
-            for(Integer num : stack) {
-                maxInStack = Math.max(maxInStack, num);
-            }
-            
-            if(maxInStack > maxNum) {
-                maxNum = maxInStack;
-                maxPos = checkPos;
+            Pos nextP = new Pos(nx, ny);
+            if (!hm.containsKey(nextP)) continue;
+            for(Integer n : hm.get(nextP)) {
+                if(n > maxNum) {
+                    maxNum = n;
+                    maxP = nextP;
+                }
             }
         }
-        
-        return maxPos;
-    }
+
+        return maxP;
+    } 
 
     private static boolean inRange(int x, int y) {
         return x>=0 && x<n && y>=0 && y<n;
@@ -95,49 +91,21 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<n; i++) {
+        for(int i=0;i<n;i++) {
             st = new StringTokenizer(bf.readLine());
-            for(int j=0; j<n; j++) {
+            for(int j=0;j<n;j++) {
                 int num = Integer.parseInt(st.nextToken());
-                
                 Pos p = new Pos(i, j);
-                if(!grid.containsKey(p)) {
-                    grid.put(p, new Stack<>());
-                }
-                
-                grid.get(p).push(num);
-                numPos.put(num, p);
+                posMap.put(num, p);
+                hm.put(p, new ArrayList<>());
+                hm.get(p).add(num);
             }
-        } 
+        }
 
         orders = new int[m];
         st = new StringTokenizer(bf.readLine());
-        for(int i=0; i<m; i++) {
+        for(int i=0;i<m;i++) {
             orders[i] = Integer.parseInt(st.nextToken());
-        }  
-    }
-
-    private static void print() {
-        
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                Pos p = new Pos(i, j);
-                
-                if(!grid.containsKey(p) || grid.get(p).isEmpty()) {
-                    System.out.println("None");
-                } else {
-                    Stack<Integer> stack = grid.get(p);
-                    StringBuilder sb = new StringBuilder();
-                    
-                    Object[] arr = stack.toArray();
-                    for(int k=arr.length-1; k>=0; k--) {
-                        if(k < arr.length-1) sb.append(" ");
-                        sb.append(arr[k]);
-                    }
-                    
-                    System.out.println(sb.toString());
-                }
-            }
         }
     }
 
@@ -153,9 +121,9 @@ public class Main {
         @Override
         public boolean equals(Object o) {
             if(o==this) return true;
-            if(o==null || o.getClass() != getClass()) return false;
+            if(o==null || getClass() != o.getClass()) return false;
             Pos p = (Pos) o;
-            return p.x==x && p.y==y;
+            return x==p.x && y==p.y;
         }
 
         @Override
